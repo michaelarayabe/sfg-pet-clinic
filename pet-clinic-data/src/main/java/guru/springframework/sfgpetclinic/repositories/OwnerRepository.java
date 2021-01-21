7c380cae15b101456a97d083c28fffc6a8e9ade7
michaelarayabe <michaelseifuaraya@gmail.com>
2021-01-21 15:47:36 +0100

    implemented repos. Closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<OwnerRepository, Long> {
+}
